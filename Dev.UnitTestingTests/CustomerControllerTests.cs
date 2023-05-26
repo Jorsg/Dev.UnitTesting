944523cba494b537ba53030943426fe9b589245b
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-25 20:24:46 -0400

    Add class Math, HtmlFormatter, CustomerController and ErrorLogger, additional also add the next test: ErrorLoggerrTest, MathTests, CustomerControllerTest and HtmlFormatterTests

---
+++ Dev.UnitTestingTests/CustomerControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Dev.UnitTesting.Fundamentals;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace Dev.UnitTestingTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new UnitTesting.Fundamentals.CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+
+            //NotFound Objet
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFoud Objet and derivados
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnOK()
+        {
+            var controller = new UnitTesting.Fundamentals.CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            //OK Objet
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+
+    }
+}
---
+++ Dev.UnitTestingTests/MathTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace Dev.UnitTestingTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private UnitTesting.Fundamentals.Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new UnitTesting.Fundamentals.Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArgument()
+        {
+            var result = _math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumersUpTolimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EqualTo(new[] {1,3,5}));
+        }
+    }
+}
