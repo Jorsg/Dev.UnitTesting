855d8caa72f31fe88f6ab81ca48dd0addddc2bce
Jorman Garcia <jagarcia@lfs-inc.com>
2023-06-28 22:46:02 -0400

    add unit test Booking

---
+++ Dev.UnitTestingTests/Moking/BookingHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using Dev.UnitTesting.Mocking;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace Dev.UnitTestingTests.Moking
+{
+	[TestFixture]
+	public class BookingHelperTests
+	{
+		private Mock<IBookingRepository> _repository;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_repository = new Mock<IBookingRepository>();
+		}
+
+
+		[Test]
+		public void OverlappingBookingsExist_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+		{
+			_repository.Setup(c => c.GetActiveBookings(1)).Returns(new List<Booking>
+			{
+				new Booking
+				{
+					ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
+					DepartureDate = new DateTime(2017, 1, 20, 10, 0,0),
+					Reference = "a"
+				}
+			}.AsQueryable());
+
+			var result = BookingHelper.OverlappingBookingsExist(new Booking
+			{
+				Id = 1,
+				ArrivalDate = new DateTime(2017, 1, 10, 14, 0, 0),
+				DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0),
+
+			}, _repository.Object);
+
+			Assert.That(result, Is.Empty);
+		}
+	}
+}
